c0c61ec913616933bbaf26081ea2f9463c33014a
Aditya Praveen <adityapraveen18@gmail.com>
2023-03-30 20:44:59 +0530

    stackimplementation

--- Stack/ImplementingStack.c
+++ Stack/ImplementingStack.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
-
-int stack[5];
+#define size 5
+int stack[size];
 int top = -1;
 int num;
 
-int push(int);
-int pop();
-int peek();
+void push()
+{
+    // to check the overflow state
+    if (top == size - 1)
+    {
+        printf("!!STACK OVERFLOW!!");
+    }
+    // to push element if theres no overflow
+    else
+    {
+        int push;
+        printf("\nEnter the element to be pushed:");
+        scanf("%d", &push);
+        top += 1; // top = top + 1;
+        stack[top] = push;
+    }
+}
+int pop()
+{
+    
+}
+
+int main()
+{
+    printf("Enter your choice:\n1}push\n2}pop\n3}peek");
+    scanf("%d", &num);
+
+    switch (num)
+    {
+    case 1:
+        push();
+        break;
+    case 2:
+        pop();
+        break;
+    case 3:
+        peek();
+        break;
+    default:
+        printf("Choose a correct option!");
+    }
+    return 0;
+}
