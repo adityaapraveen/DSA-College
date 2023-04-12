6d5e2826193ce9c229981622afe15a4eb0d3684b
Aditya Praveen <adityapraveen18@gmail.com>
2023-04-12 20:09:22 +0530

    hi

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,19 @@
+ for
+//   {
+//       printf();
+//   }
+//   printf("\nEnter the array elements: ");
+//   for (int i = 0; i < num_elements; i++)
+//   {
+//       printf("\nEnter the %dth element: ", i);
+//       scanf("%d", &iarray[i]);
+//   }
+
+//   printf("\n ");
+//   printf("\nDisplay the array elements");
+//   for (int i = 0; i < num_elements; i++)
+//   {
+//       printf("\nElement at %d is %d", i, iarray[i]);
+//       printf("\nAddress of %dth element is %p", i, &iarray[i]);
+//   }
+// }
